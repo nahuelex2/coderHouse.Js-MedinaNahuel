820e59f3d97fed9d5eb9dc05f81b796176a5c656
Nahuel Medina <nahuelmedina738@gmail.com>
2023-09-15 14:01:42 -0300

    agregado de slider de industrias

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-import { industryCategories } from "./industries.js";
-import { stores } from "./stores.js"
+// import { industryCategories } from "./industries.js";
+// import { stores } from "./stores.js"
 
-let industries = industryCategories.map((industry, index) => {
+// let industries = industryCategories.map((industry, index) => {
 
-    return {
-        industry,
-        orden: index + 1
-    }
-}).map(industry => `  ${industry.orden}) ${industry.industry.name}`)
-let id_industry = 0
-let value = parseInt(prompt(`Escribe  el  numero para ingresar  al rubro  deseado  \n ${industries}`))
-switch (value) {
-    case 1:
+//     return {
+//         industry,
+//         orden: index + 1
+//     }
+// }).map(industry => `  ${industry.orden}) ${industry.industry.name}`)
+// let id_industry = 0
+// let value = parseInt(prompt(`Escribe  el  numero para ingresar  al rubro  deseado  \n ${industries}`))
+// switch (value) {
+//     case 1:
 
-        id_industry = industryCategories.find(industry => industry.name == 'Restaurantes').id
-        break;
-    case 2:
-        id_industry = industryCategories.find(industry => industry.name == 'Farmacias').id
-        break;
-    case 3:
-        id_industry = industryCategories.find(industry => industry.name == 'Mascotas').id
-        break;
-    default:
-        break;
-}
+//         id_industry = industryCategories.find(industry => industry.name == 'Restaurantes').id
+//         break;
+//     case 2:
+//         id_industry = industryCategories.find(industry => industry.name == 'Farmacias').id
+//         break;
+//     case 3:
+//         id_industry = industryCategories.find(industry => industry.name == 'Mascotas').id
+//         break;
+//     default:
+//         break;
+// }
 
-const getStores = stores.filter(store => store.industryCategory == id_industry)
-let test = ``
+// const getStores = stores.filter(store => store.industryCategory == id_industry)
+// let test = ``
 
-getStores.forEach(store => {
-    test += `${store.name}\n\n`
-    store.products.sort(prod => prod.category)
+// getStores.forEach(store => {
+//     test += `${store.name}\n\n`
+//     store.products.sort(prod => prod.category)
 
-    store.products.forEach(product => {
-
-
-
-        test += `categoria: ${product.category}\n ${product.name}: ${product.price} \n \n`
+//     store.products.forEach(product => {
 
 
 
-    })
+//         test += `categoria: ${product.category}\n ${product.name}: ${product.price} \n \n`
 
-    test += `----------------\n`
-})
 
-alert(test)
+
+//     })
+
+//     test += `----------------\n`
+// })
+
+// alert(test)
